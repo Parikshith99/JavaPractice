022de81f3c352cfcacc03203d2f47efb935f425f
Parikshith Acharya <paacharya@teksystems.com>
2022-03-29 13:04:23 +0530

    Added modifier added

---
+++ src/com/java/oop/Modifiers.java
@@ -0,0 +1,12 @@
+package com.java.oop;
+
+public class Modifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("PUBLIC");
+        System.out.println("PRIVATE");
+        System.out.println("PROTECTED");
+	}
+
+}
